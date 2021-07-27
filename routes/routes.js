356d58d6b556d5e95dcaeab5d90d73b34639e3d6
fabriziocarella <fabacasa@gmail.com>
2021-07-27 15:34:04 +0200

    routes update put and delete

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const pages = require("../controllers/pages");
 
 router.get("/films/:title?", pages.getFilms);
 router.post("/films/", pages.postFilms)
+router.put("/films/", pages.putFilms)
+router.delete("/films/", pages.deleteFilms)
+
 
 
 module.exports = router;
