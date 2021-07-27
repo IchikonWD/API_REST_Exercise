80dcbfb6a378beb8c8ce4070465c5adf02a10960
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-07-27 15:06:33 +0200

    error handle created

--- controllers/pages.js
+++ controllers/pages.js
@@ -6,6 +6,12 @@ const pages = {
     const response = await film.getMovieByTitle(title);
     res.status(201).json(response);
   },
+  notFound404: (req, res, next) => {
+    return res.status(404).send({ message: "Route" + req.url + " Not found." });
+  },
+  notFound500: (req, res, next)=> {
+    return res.status(500).send({ error: err });
+  },
 };
 
 module.exports = pages;
