0aad1ffbd7eb0a9828e7cb6c15c393fd72002357
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-07-27 12:40:05 +0200

    app.js defined

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
