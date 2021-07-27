b3bbb8676dd5330e827325952ec6b4e0ca8a7742
fabriziocarella <fabacasa@gmail.com>
2021-07-27 14:37:15 +0200

    utils update

--- utils/films.js
+++ utils/films.js
@@ -1,6 +1,12 @@
 const fetch = require("node-fetch");
 const key = process.env.API_KEY
 
-const film = {};
-
+const film = {
+    getMovieByTitle: async (title) => {
+        const res = await fetch(`http://www.omdbapi.com/?t=${title}&apikey=${key}&`)
+        const data = await res.json()
+        console.log(data);
+        return data
+    }
+};
 module.exports = film
